5c58b939efa7f3bddd4e5f3c7dfd4458ca9d5ce2
Beto <luisrobertoantunes@gmail.com>
2022-08-22 23:24:22 -0300

    Primeiro Codigo em C

---
+++ Aula 01/Ola-Mundo.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //Biblioteca necessária para entrada e saída de dados
+#include <stdlib.h> //Necessário para o system ("Pause")
+
+//Função Principal
+int main (void){ //Início do Programa
+	
+	printf("Hello Word!\n"); //Exibe uma Mensage na tela
+	
+	system("Pause");
+	return 0; //Valor de retorno da função main - Zero significa que executou com sucesso
+}//Fim do Programa
---
+++ Ola-Mundo.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //Biblioteca necessária para entrada e saída de dados
+
+//Função Principal
+int main (void){ //Início do Programa
+	printf("Hello Word!");
+	
+	return 0; //Valor de retorno da função main - Zero significa que executou com sucesso
+}//Fim do Programa
