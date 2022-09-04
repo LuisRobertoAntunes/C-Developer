93833f361caf99f5e2c327a139c14a210dd39d7d
Beto <luisrobertoantunes@gmail.com>
2022-09-04 00:28:46 -0300

    Estrutura de repetição While

---
+++ Aula 23/RepeticaoWhile.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (void){
+	
+	int contador = 1; //declarando e iniciando o variável de controle
+	
+	while (contador <= 100){ //testando a condição
+	printf("%d ",contador); //executando um comando dentro do laço
+	
+	//contador++; //atualizando a variável de controle
+	}
+	
+	return 0;
+}
+
+/*
+	while (condição)
+	{
+	instrução ou bloco de instruções
+	}
+*/
