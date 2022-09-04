de7b430cb4bbea357c86a1bf4df301a2667a4601
Beto <luisrobertoantunes@gmail.com>
2022-09-03 23:51:55 -0300

    Estrutura de repetição For decrescente

---
+++ Aula 21/ForDecrescente.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void){
+	
+	int contador;
+	
+	//Exemplo de laço com contagem decrescente
+	for(contador = 10; contador >= 0; contador--){
+		printf("%d ", contador);
+	}
+	
+	return 0;
+}
