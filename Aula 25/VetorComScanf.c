6ad90c56b189d34bc2326f3886ab33894cbe3ce8
Beto <luisrobertoantunes@gmail.com>
2022-09-04 23:26:20 -0300

    Recebendo dados com scanf para um array

---
+++ Aula 25/VetorComScanf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void){
+	
+	float notas[4];
+	int i;
+	
+	//entrada de dados
+	for(i = 0; i <= 3; i++){
+	printf("Digite a nota do aluno: ");
+	scanf("%f", &notas[i]);
+	}
+	
+	//saída de dados
+	for(i = 0; i <= 3; i++){
+		printf("Valor da nota %d: %.1f \n",i,notas[i]);
+	}
+	
+	return 0;
+}
