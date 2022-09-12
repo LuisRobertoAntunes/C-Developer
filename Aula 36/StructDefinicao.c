bb88be11e9d8e8e032a3446d7774730985373e81
Beto <luisrobertoantunes@gmail.com>
2022-09-11 22:07:56 -0300

    Struct - Definição

---
+++ Aula 36/StructDefinicao.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main (){
+	
+	//Definção da Struct
+	struct ficha_aluno{ // Tipo de dado
+		
+		char nome[40];
+		int numero;
+		float nota;
+	};
+	
+	struct ficha_aluno aluno;
+	
+	printf("\n---------- Cadastro de aluno ----------\n\n\n");
+	
+	printf("Nome do aluno .....: ");
+	fgets(aluno.nome, 40,stdin);
+	
+	printf("Digite o numero do aluno: ");
+	scanf("%d",&aluno.numero);
+	
+	printf("Informe a nota do aluno: ");
+	scanf("%f",&aluno.nota);
+	
+	printf("\n\n---------- Lendo os dados da struct ----------\n\n");
+	
+	printf("Nome ............: %s", aluno.nome);
+	printf("Numero ..........: %d\n", aluno.numero);
+	printf("Nota ............: %.2f\n", aluno.nota);
+	
+	return 0;
+}
